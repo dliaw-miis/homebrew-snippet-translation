dad691d0dd17973082e52a5179f5870ab9e366ee
koiabi <koiabi8888@gmail.com>
2022-05-07 15:41:33 -0700

    Populate data.js

--- tl_data/data.js
+++ tl_data/data.js
@@ -1,6 +1,131 @@
 const data = {
   "http://127.0.0.1:3000/test_page/Home%20Page.htm": {
-
+      "P,\n    ,FONT,IMG,\n    ,FONT,\n    ,FONT,\n  , ,     ,\n      ,FONT,IMG,\n    ,The OFFICIAL ": {
+          "textContent": "\n     \n         \n    \n      The OFFICIAL \n    \n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"5\">&nbsp;</font><img src=\"images/nojordon.jpg\" alt=\"nojordon.jpg (4614 bytes)\" width=\"75\" height=\"90\">\n    <font size=\"5\">&nbsp;&nbsp;&nbsp;&nbsp; </font>\n    <font face=\"Comic Sans MS\">\n      <font size=\"6\">The OFFICIAL </font><img src=\"images/nojordon.jpg\" alt=\"nojordon.jpg (4614 bytes)\" width=\"75\" height=\"90\">\n    </font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,\n      ,FONT,\n      ,FONT,\n      ,FONT,\n    ,Exposers of jordon's,  ,Foolishness Page": {
+          "textContent": "\n    \n      Exposers of jordon's\n        \n      Foolishness Page\n    \n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font face=\"Comic Sans MS\">\n      <font size=\"6\">Exposers of jordon's</font>\n      <font size=\"5\">&nbsp;&nbsp;</font>\n      <font size=\"6\">Foolishness Page</font>\n    </font>\n  </p>"
+          }
+      },
+      "P,BR,\n    ,FONT,\n  ,The sister page of the ,A,Michael\n        Jordan - NOT the best page": {
+          "textContent": "\n    The sister page of the Michael\n        Jordan - NOT the best page\n  ",
+          "translation": {
+              "ja": "<p align=\"center\"><br>\n    <font face=\"Comic Sans MS\" size=\"3\">The sister page of the <a href=\"../jordan/index.htm\" target=\"_parent\">Michael\n        Jordan - NOT the best page</a></font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,\"Having jordon on your team is\n      like spotting the other team points\" -- Air Judden": {
+          "textContent": "\n    \"Having jordon on your team is\n      like spotting the other team points\" -- Air Judden\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font face=\"Comic Sans MS\" size=\"3\">\"Having jordon on your team is\n      like spotting the other team points\" -- Air Judden</font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,\"The last time anyone travelled\n      as much as [jordon], they discovered the New World.\" -- Colt Fortyfeinberg": {
+          "textContent": "\n    \"The last time anyone travelled\n      as much as [jordon], they discovered the New World.\" -- Colt Fortyfeinberg\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"3\" face=\"Comic Sans MS\">\"The last time anyone travelled\n      as much as [jordon], they discovered the New World.\" -- Colt Fortyfeinberg</font>\n  </p>"
+          }
+      },
+      "P,FONT,\n  ,FONT,\n  ,\n  ,The ONLY webpage officially\n    blessed by ,A,SUP,Mathgod,TM": {
+          "textContent": "\n  \n  The ONLY webpage officially\n    blessed by MathgodTM\n  ",
+          "translation": {
+              "ja": "<p align=\"center\"><font size=\"2\">\n  </font>\n  <font size=\"4\" face=\"Comic Sans MS\">The ONLY webpage officially\n    blessed by <a href=\"wordsofwisdom.htm#mathgod\">Mathgod</a><sup>TM</sup></font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,If there is not a black navigation\n      panel down the left hand side of the screen, click ,A,.   \n    ,here": {
+          "textContent": "\n    If there is not a black navigation\n      panel down the left hand side of the screen, click here.   \n    \n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"4\" face=\"Comic Sans MS\">If there is not a black navigation\n      panel down the left hand side of the screen, click <a href=\"indexf.html\" target=\"_parent\">here</a>. &nbsp;&nbsp;\n    </font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,A, New?,What's": {
+          "textContent": "\n    What's New?\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"6\" face=\"Comic Sans MS\"><a name=\"whatsnew\">What's</a> New?</font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n    ,FONT,\n  ,  \n    ,BR,\n      This site is no longer updated.": {
+          "textContent": "\n      \n    \n    \n      This site is no longer updated.\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font face=\"Comic Sans MS\">&nbsp;&nbsp;\n    </font>\n    <font size=\"4\"><br>\n      This site is no longer updated.</font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,Vintage jordon -- getting schooled by a\n      rookie!": {
+          "textContent": "\n    Vintage jordon -- getting schooled by a\n      rookie!\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font face=\"Comic Sans MS\">Vintage jordon -- getting schooled by a\n      rookie!</font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,\n      ,FONT,\n      ,FONT,\n      ,FONT,\n      ,FONT,\n      ,FONT,\n      ,FONT,\n      ,FONT,\n    ,Welcome to the ONLY page on the\n        internet that tells the TRUTH about Michael jordon. Many pages jock him unnecessarily, and\n        I have never understood these peoples infatuation with a player of such limited ability\n        and such negative character traits. ,BR,\n        ,BR,\n        Michael jordon is a foolish and wretched 1-dimensional ballhog. However, many people think\n        he is the greatest basketball player in history. These people are mindless Borg-like\n        robots sharing a collective pea-brain and are fed their thoughts from the\n      ,N,ational ,B,ulls ,C,ompany via a TV interface, every Sunday.": {
+          "textContent": "\n    \n      Welcome to the ONLY page on the\n        internet that tells the TRUTH about Michael jordon. Many pages jock him unnecessarily, and\n        I have never understood these peoples infatuation with a player of such limited ability\n        and such negative character traits. \n        \n        Michael jordon is a foolish and wretched 1-dimensional ballhog. However, many people think\n        he is the greatest basketball player in history. These people are mindless Borg-like\n        robots sharing a collective pea-brain and are fed their thoughts from the\n      \n      N\n      ational \n      B\n      ulls \n      C\n      ompany via a TV interface, every Sunday.\n    \n  ",
+          "translation": {
+              "ja": "<p align=\"left\">\n    <font face=\"Comic Sans MS\">\n      <font size=\"4\">Welcome to the ONLY page on the\n        internet that tells the TRUTH about Michael jordon. Many pages jock him unnecessarily, and\n        I have never understood these peoples infatuation with a player of such limited ability\n        and such negative character traits. <br>\n        <br>\n        Michael jordon is a foolish and wretched 1-dimensional ballhog. However, many people think\n        he is the greatest basketball player in history. These people are mindless Borg-like\n        robots sharing a collective pea-brain and are fed their thoughts from the\n      </font>\n      <font size=\"5\" color=\"#FF0000\">N</font>\n      <font size=\"4\">ational </font>\n      <font size=\"5\" color=\"#FF0000\">B</font>\n      <font size=\"4\">ulls </font>\n      <font size=\"5\" color=\"#FF0000\">C</font>\n      <font size=\"4\">ompany via a TV interface, every Sunday.</font>\n    </font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,Hopefully, you have not been\n      assimilated by the jordon collective.  If so, then you can use your wonderful power\n      of reasoning to see that jordon is indeed the foolish ballhog I have exposed him to be. ": {
+          "textContent": "\n    Hopefully, you have not been\n      assimilated by the jordon collective.  If so, then you can use your wonderful power\n      of reasoning to see that jordon is indeed the foolish ballhog I have exposed him to be. \n  ",
+          "translation": {
+              "ja": "<p align=\"left\">\n    <font size=\"4\" face=\"Comic Sans MS\">Hopefully, you have not been\n      assimilated by the jordon collective.&nbsp; If so, then you can use your wonderful power\n      of reasoning to see that jordon is indeed the foolish ballhog I have exposed him to be. </font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,Click on the logo to read about the\n      legend of the Blair jordon Ballhog who has been haunting the woods of North Carolina for\n      nearly a century!": {
+          "textContent": "\n    Click on the logo to read about the\n      legend of the Blair jordon Ballhog who has been haunting the woods of North Carolina for\n      nearly a century!\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"4\" face=\"Comic Sans MS\">Click on the logo to read about the\n      legend of the Blair jordon Ballhog who has been haunting the woods of North Carolina for\n      nearly a century!</font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,Episode 1: The Ballhog Menace": {
+          "textContent": "\n    Episode 1: The Ballhog Menace\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"6\" face=\"Comic Sans MS\">Episode 1: The Ballhog Menace</font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,In April, cable TV brought us\n      \"The Michael jordon story.\"  Now, you can read the pre-quel.   Learn\n      how Foolish jordon went from a potentially great prep-school star to a foolish and\n      wretched 1-dimensional ballhog.  Click on the poster to read the story!": {
+          "textContent": "\n    In April, cable TV brought us\n      \"The Michael jordon story.\"  Now, you can read the pre-quel.   Learn\n      how Foolish jordon went from a potentially great prep-school star to a foolish and\n      wretched 1-dimensional ballhog.  Click on the poster to read the story!\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"4\" face=\"Comic Sans MS\">In April, cable TV brought us\n      \"The Michael jordon story.\"&nbsp; Now, you can read the pre-quel. &nbsp; Learn\n      how Foolish jordon went from a potentially great prep-school star to a foolish and\n      wretched 1-dimensional ballhog.&nbsp; Click on the poster to read the story!</font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,This is the Official Home for the\n      campaign to elect jordon Cooke County dog catcher.  To read more, click on the\n      poster.  This section will be updated as the campaign intensifies.  Last updated\n      Feb 23, 1999.": {
+          "textContent": "\n    This is the Official Home for the\n      campaign to elect jordon Cooke County dog catcher.  To read more, click on the\n      poster.  This section will be updated as the campaign intensifies.  Last updated\n      Feb 23, 1999.\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"4\" face=\"Comic Sans MS\">This is the Official Home for the\n      campaign to elect jordon Cooke County dog catcher.&nbsp; To read more, click on the\n      poster.&nbsp; This section will be updated as the campaign intensifies.&nbsp; Last updated\n      Feb 23, 1999.</font>\n  </p>"
+          }
+      },
+      "P,\n    ,\n    ,\n    ,\n    ,BR,\n    ,\n    ,\n    ,BR,\n    ,\n    ,FONT,\n\n  ,BR,\n      Have become enlightened since Feb 19, 1998,BR,\n    ": {
+          "textContent": "\n    \n    \n    \n    \n    \n    \n    \n    \n    \n      Have become enlightened since Feb 19, 1998\n    \n\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <!-- BEGIN FASTCOUNTER CODE -->\n    <!-- <a href=\"http://member.bcentral.com/cgi-bin/fc/fastcounter-login?1752651\" target=\"_top\">\n      <img border=\"0\" src=\"http://fastcounter.bcentral.com/fastcounter?1752651+3505309\"></a> -->\n    <!-- END FASTCOUNTER CODE -->\n    <br>\n    <!-- BEGIN FASTCOUNTER LINK -->\n    <!-- <font face=\"arial\" size=\"1\">\n      <a href=\"http://fastcounter.bcentral.com/fc-join\" target=\"_top\">FastCounter by bCentral</a> -->\n    <br>\n    <!-- END FASTCOUNTER LINK -->\n    <font face=\"Comic Sans MS\"><br>\n      Have become enlightened since Feb 19, 1998<br>\n    </font>\n\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,jordon's brick counter,BR,\n    ": {
+          "textContent": "\n    jordon's brick counter\n    \n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font face=\"Comic Sans MS\">jordon's brick counter<br>\n    </font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,Want to join the good fight?  \n      Feel free to copy this banner and paste it on your website.  Please link it to:\n      https://airjudden2.tripod.com/ejf/": {
+          "textContent": "\n    Want to join the good fight?  \n      Feel free to copy this banner and paste it on your website.  Please link it to:\n      https://airjudden2.tripod.com/ejf/\n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"4\" face=\"Comic Sans MS\">Want to join the good fight? &nbsp;\n      Feel free to copy this banner and paste it on your website.&nbsp; Please link it to:\n      https://airjudden2.tripod.com/ejf/</font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,DISCLAIMER,BR,\n    ": {
+          "textContent": "\n    DISCLAIMER\n    \n  ",
+          "translation": {
+              "ja": "<p align=\"center\">\n    <font size=\"6\" face=\"Comic Sans MS\">DISCLAIMER<br>\n    </font>\n  </p>"
+          }
+      },
+      "P,\n    ,FONT,\n  ,I am NOT Michael jordon. I know it seems odd\n      that a person could know so much about jordon's foolishness and not BE Foolish jordon, but\n      I guarantee, I am not.  Therefore, don't write questions asking about why I am a\n      ballhog, or if you are a jordon jocker, please don't write asking if you may smell my\n      underwear.  I am not a ballhog and I don't wear jordon's drawers!": {
+          "textContent": "\n    I am NOT Michael jordon. I know it seems odd\n      that a person could know so much about jordon's foolishness and not BE Foolish jordon, but\n      I guarantee, I am not.  Therefore, don't write questions asking about why I am a\n      ballhog, or if you are a jordon jocker, please don't write asking if you may smell my\n      underwear.  I am not a ballhog and I don't wear jordon's drawers!\n  ",
+          "translation": {
+              "ja": "<p align=\"left\">\n    <font face=\"Comic Sans MS\">I am NOT Michael jordon. I know it seems odd\n      that a person could know so much about jordon's foolishness and not BE Foolish jordon, but\n      I guarantee, I am not.&nbsp; Therefore, don't write questions asking about why I am a\n      ballhog, or if you are a jordon jocker, please don't write asking if you may smell my\n      underwear.&nbsp; I am not a ballhog and I don't wear jordon's drawers!</font>\n  </p>"
+          }
+      }
   }
 }
 
